98e067aa81f3c743ddf73643abc1f34c2af2076b
Gwiilo <41574564+Gwiilo@users.noreply.github.com>
2021-08-30 00:58:29 +0100

    Dependencies + gradle properties

---
+++ src/main/java/net/gwiilo/gonut/gonut.java
@@ -0,0 +1,10 @@
+package net.gwiilo.gonut;
+
+import net.fabricmc.api.ModInitializer;
+
+public class gonut implements ModInitializer {
+    @Override
+    public  void onInitialize() {
+        System.out.print("Hi!");
+    }
+}
