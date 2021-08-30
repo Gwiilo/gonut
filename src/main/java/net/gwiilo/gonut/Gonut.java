6900e62b01dd173fe580e61346f12c8fdc18ccec
Gwiilo <41574564+Gwiilo@users.noreply.github.com>
2021-08-30 02:02:37 +0100

    Initialization
    Initial mod skeleton in place, now to organ it up baby

---
+++ src/main/java/net/gwiilo/gonut/Gonut.java
@@ -0,0 +1,16 @@
+package net.gwiilo.gonut;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gonut implements ModInitializer {
+    @Override
+    public  void onInitialize() {
+
+    }
+
+    /*note(Daenyl)
+    for this first mod, let's try adding keybind R zooms the camera in
+     */
+
+}
+
